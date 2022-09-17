b40d354b94e74f094ad95716a0aa472828136243
Cole <letchro@gmail.com>
2022-09-17 16:55:40 -0500

    Testing all APIs that will be used for website

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 
 async function getAddress() {
     const response = await fetch("http://ip.jsontest.com/");
+    const response2 = await fetch("http://headers.jsontest.com/");
+
+    Promise.all([
+        fetch('http://ip.jsontest.com/').then(resp => resp.json()),
+        fetch('http://headers.jsontest.com/').then(resp => resp.json()),
+        fetch('http://date.jsontest.com').then(resp => resp.json()),
+        fetch('http://validate.jsontest.com/?json={"key":"value"}').then(resp => resp.json()),
+        fetch('http://md5.jsontest.com/?text=example_text').then(resp => resp.json()),
+    ]).then(console.log);
+
     if (response.ok === false) {
         console.log("Fetch ERROR");
         return null;
     }
     const data = await response.json();
-    console.log(data);
-    return data;
+    const data2 = await response2.json();
+    console.log(data, data2);
+    return (data, data2);
 }
 
 getAddress();
