880acb973b0351cf9332e2c814c7eba958a73c0a
Cole <letchro@gmail.com>
2022-09-19 10:19:24 -0500

    Added display for API and headers

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
+const address = document.getElementById("address");
+const headers = document.getElementById("headers");
+const dateAndTime = document.getElementById("dateAndTime");
+const jsonCheck = document.getElementById("jsonCheck");
+const md5Calc = document.getElementById("md5Calc");
 
 async function getAddress() {
     const response = await fetch("http://ip.jsontest.com/");
     const response2 = await fetch("http://headers.jsontest.com/");
 
-    Promise.all([
-        fetch('http://ip.jsontest.com/').then(resp => resp.json()),
-        fetch('http://headers.jsontest.com/').then(resp => resp.json()),
-        fetch('http://date.jsontest.com').then(resp => resp.json()),
-        fetch('http://validate.jsontest.com/?json={"key":"value"}').then(resp => resp.json()),
-        fetch('http://md5.jsontest.com/?text=example_text').then(resp => resp.json()),
-    ]).then(console.log);
+    // Promise.all([
+    //     fetch('http://ip.jsontest.com/').then(resp => resp.json()),
+    //     fetch('http://headers.jsontest.com/').then(resp => resp.json()),
+    //     fetch('http://date.jsontest.com').then(resp => resp.json()),
+    //     fetch('http://validate.jsontest.com/?json={"key":"value"}').then(resp => resp.json()),
+    //     fetch('http://md5.jsontest.com/?text=example_text').then(resp => resp.json()),
+    // ]).then(console.log);
 
     if (response.ok === false) {
         console.log("Fetch ERROR");
@@ -17,8 +22,21 @@ async function getAddress() {
     }
     const data = await response.json();
     const data2 = await response2.json();
+
+    await display(JSON.stringify(data), JSON.stringify(data2));
     console.log(data, data2);
-    return (data, data2);
+
 }
 
 getAddress();
+
+async function display(data, data2) {
+    try {
+        address.innerHTML += `${data}`;
+        headers.innerHTML += `${data2}`;
+
+    } catch (error) {
+        console.log("ERROR display");
+        return null;
+    }
+}
